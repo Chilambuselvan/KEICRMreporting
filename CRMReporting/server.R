6f2c2f15c9f3ad441fc6cb18f9f73f0c6b5b1ee2
Chilambuselvan <chilambuselvan@gmail.com>
2016-09-09 03:03:36 +0530

    Dashboard Complete

--- CRMReporting/server.R
+++ CRMReporting/server.R
@@ -12,15 +12,43 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
    
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+   reactdataCon=reactive({
+     subcon=OppClosed%>%
+       filter(Market.segment==input$MarSegChoose)
   })
+   
+   reactdataConSum = reactive({
+     subcon1=OppClosed%>%
+       filter(Market.segment==input$MarSegChoose)%>%
+       group_by(Region)%>%
+       summarise(cnt=sum(Won))
+   })
+  ################################ DASHBOARD TAB ########################  
+  output$ClosedSuccess <- renderValueBox({
+    StageCounts=count(reactdataCon(),Stage)
+    #   perct=subset(StageCounts,Stage=="Order Received (Won)")$n/nrow(reactdataCon())
+       valueBox(
+         paste0(subset(StageCounts,Stage=="Order Received (Won)")$n, " Nos "), "WON", icon = icon("list"),
+         color = "purple"
+       )
+    
+    })
+   output$ClosedSuccessPer <- renderValueBox({
+    StageCounts=count(reactdataCon(),Stage)
+    perct=subset(StageCounts,Stage=="Order Received (Won)")$n/nrow(reactdataCon())
+    valueBox(
+      paste0(round(perct*100,0), " %"), "WON", icon = icon("thumbs-up", lib = "glyphicon"),
+      color = "aqua"
+    )
+  })
+   
+   output$DashSuccessChart<-renderPlotly({
+     plot_ly(data=reactdataConSum(),x = Region ,y = cnt,showlegend=FALSE,type="bar")%>%
+     add_trace(x = Region, y=cnt,text=cnt,mode="text",textposition ="top middle",
+               showlegend=FALSE,hoverinfo="none")%>%
+      layout(xaxis=list(title = "Region"),yaxis=list(title = "Quantity"))%>%
+       layout(title="Opportunity Won Region Wise")
+     #rm(subdf)
+   })
   
 })
--- CRMReporting/ui.R
+++ CRMReporting/ui.R
@@ -5,29 +5,72 @@
 # Find out more about building applications with Shiny here:
 # 
 #    http://shiny.rstudio.com/
+#   https://rstudio.github.io/shinydashboard/get_started.html
 #
-library(data.table)
+library(shinydashboard)
+#library(data.table)
+library(openxlsx)
+library(tidyr)
+library(dplyr)
+library(plotly)
+#options(java.parameters = "-Xmx1024m" )
 library(shiny)
-=
+
+#setwd("F:/Official/KEICRMreporting/CRMReporting/Data")
+if (FALSE){
+  OppClosed = read.xlsx("closed.xlsx",sheet = 1,startRow = 1, colNames = TRUE,
+                         detectDates = TRUE,rowNames = FALSE)
+  OppOpen = read.xlsx("open.xlsx",sheet = 1,startRow = 1, colNames = TRUE,
+                        detectDates = TRUE,rowNames = FALSE)
+  ClosedMarketSeg=unique(OppClosed$Market.segment)
+  OppClosed$Won=ifelse(OppClosed$Stage=="Order Received (Won)",1,0)
+  
+}
+
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+shinyUI(
+  dashboardPage(
+    dashboardHeader(title = "KEI CRM Reporting"),
+    dashboardSidebar(
+      sidebarMenu(
+        menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+        menuItem("OPEN opportunities", tabName = "widgets", icon = icon("th"))
+       # menuItem("Closed opportunities", tabName = "widgets2", icon = icon("th"))
+      )
     ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
-  )
-))
+    dashboardBody(
+      tabItems(
+        # First tab content
+        tabItem(tabName = "dashboard",
+                fluidRow(
+                  #valueBoxOutput("ThisMonth"),
+                  selectInput(inputId = "MarSegChoose","Market Segment",ClosedMarketSeg,multiple = FALSE),
+                  # Dynamic valueBoxes
+                 # valueBox(10 * 2, "New Orders", icon = icon("credit-card")),
+                  valueBoxOutput("ClosedSuccess"),
+                  
+                 valueBoxOutput("ClosedSuccessPer")
+                  
+                ),#fluidrow ends
+                fluidRow(
+                  box(plotlyOutput("DashSuccessChart", height = 350),status = "success"),
+                  
+                  box(
+                    title = "Controls",
+                    sliderInput("slider", "Number of observations:", 1, 100, 50)
+                  )
+                )#Fluid row ends
+        ),
+        
+        # Second tab content
+        tabItem(tabName = "widgets",
+                h2("Widgets tab content")
+        )
+      )
+      
+    )#dashboard Body Close
+  )#dashboard page close
+  
+ 
+  
+)
